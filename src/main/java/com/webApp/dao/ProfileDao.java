65415012548df1c07576ac9db216e043d6beb900
Aliksei Tkachuk <aliksei_tkachuk@epam.com>
2017-05-24 14:48:55 +0300

    now it works

--- src/main/java/com/webApp/dao/ProfileDao.java
+++ src/main/java/com/webApp/dao/ProfileDao.java
@@ -1,6 +1,15 @@
 package com.webApp.dao;
 
+import com.webApp.entities.Ticket;
+
+import java.util.List;
+
 public interface ProfileDao {
 
+    void createProfile(String userName);
+
+    void addTicketToProfile(String userName, Ticket ticket);
+
+    List<Ticket> getTickets(final String userName);
 
 }
